7048f4e87f4f0cc25edf715d94c717455472570f
shellywilliamsreichert802 <shellyreichert802@gmail.com>
2024-05-03 11:44:38 -0500

    Assignment #2 - Task 2 - All 9 tests passed

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -15,7 +15,11 @@ public class Location {
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
-
+    // Constructor that takes a string parameter
+    public Location(String value) {
+        this(); // Call the empty constructor to initialize id
+        this.value = value;
+    }
 
     // Custom toString, equals, and hashCode methods:
 
