f31123e3921da0793917e2d543e99ac1858ba251
shellywilliamsreichert802 <shellyreichert802@gmail.com>
2024-05-13 19:55:43 -0500

    Assignment #2 - Task 4 - All 3 tests passed

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -7,6 +7,10 @@ public class Job {
     private int id;
     private static int nextId = 1;
 
+    public int getId() {
+        return id;
+    }
+
     private String name;
     private Employer employer;
     private Location location;
@@ -21,6 +25,11 @@ public class Job {
     public Job() {
         id = nextId;
         nextId++;
+        name = "";
+        employer = new Employer("");
+        location = new Location("");
+        positionType = new PositionType("");
+        coreCompetency = new CoreCompetency("");
     }
     // Constructor with 5 parameters
     public Job(String name, Employer employer, Location location,
@@ -32,24 +41,68 @@ public class Job {
         this.positionType = positionType;
         this.coreCompetency = coreCompetency;
     }
+
+
+    // toString method
+    @Override
+    public String toString() {
+        // Start with blank line it separates previous text
+        String result = System.lineSeparator();
+        // adds _(ie.ID/Name/etc) field label and value then a new line
+        result += "ID: " + this.getId() + System.lineSeparator();
+        // when empty "Data not available" then new line
+        result += "Name: " + (this.getName().isEmpty() ? "Data not available" : this.getName()) + System.lineSeparator();
+        result += "Employer: " + (this.getEmployer().getValue().isEmpty() ? "Data not available" : this.getEmployer().getValue()) + System.lineSeparator();
+        result += "Location: " + (this.getLocation().getValue().isEmpty() ? "Data not available" : this.getLocation().getValue()) + System.lineSeparator();
+        result += "Position Type: " + (this.getPositionType().getValue().isEmpty() ? "Data not available" : this.getPositionType().getValue()) + System.lineSeparator();
+        result += "Core Competency: " + (this.getCoreCompetency().getValue().isEmpty() ? "Data not available" : this.getCoreCompetency().getValue()) + System.lineSeparator();
+        // Add a blank line to end to separate from following text
+        // result += System.lineSeparator();
+
+        return result;
+    }
+
+//    public String toString() {
+//        return System.lineSeparator() +
+//                "ID: " + this.getId() + System.lineSeparator() +
+//                "Name: Data not available" + System.lineSeparator() +
+//                "Employer: Data not available" + System.lineSeparator() +
+//                "Location: Data not available" + System.lineSeparator() +
+//                "Position Type: Data not available" + System.lineSeparator() +
+//                "Core Competency: Data not available" + System.lineSeparator();
+//    }
+
+
+
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
-    @Override // Custom equals() method
+    @Override
     public boolean equals(Object o) {
+        // Check if the object being compared is the same as the current object
         if (this == o) return true;
-        if (!(o instanceof Job job)) return false;
-        Job otherJob = (Job) o;
-        return getId() == otherJob.getId();
+        // Check if the object being compared is null or belongs to a different class
+        if (o == null || getClass() != o.getClass()) return false;
+        // Cast the object being compared to a Job object
+        Job job = (Job) o;
+        // Compare the id of the current object with the id of the object being compared
+        return id == job.id;
+        //Only need to check for same id. Nothing else. So don't need code below.
+        //== job.id && Objects.equals(name, job.name) && Objects.equals(employer, job.employer) && Objects.equals(location, job.location) && Objects.equals(positionType, job.positionType) && Objects.equals(coreCompetency, job.coreCompetency);
     }
 
-    @Override // Custom hashCode() method
+
+    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
+    //  match.
+    @Override
+    //The hashCode method is overridden to generate a hash code based on the id field of the Job class
     public int hashCode() {
-        return Objects.hash(getId());
+        return Objects.hash(id);
     }
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
+
 
     public String getName() {
         return name;
@@ -91,7 +144,80 @@ public class Job {
         this.coreCompetency = coreCompetency;
     }
 
-    public int getId() {
-        return id;
-    }
 }
+
+//
+//    @Override // Custom equals() method
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof Job job)) return false;
+//        Job otherJob = (Job) o;
+//        return getId() == otherJob.getId();
+//    }
+//
+//    @Override // Custom hashCode() method
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
+//
+//    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
+//    //  and id.
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Employer getEmployer() {
+//        return employer;
+//    }
+//
+//    public void setEmployer(Employer employer) {
+//        this.employer = employer;
+//    }
+//
+//    public Location getLocation() {
+//        return location;
+//    }
+//
+//    public void setLocation(Location location) {
+//        this.location = location;
+//    }
+//
+//    public PositionType getPositionType() {
+//        return positionType;
+//    }
+//
+//    public void setPositionType(PositionType positionType) {
+//        this.positionType = positionType;
+//    }
+//
+//    public CoreCompetency getCoreCompetency() {
+//        return coreCompetency;
+//    }
+//
+//    public void setCoreCompetency(CoreCompetency coreCompetency) {
+//        this.coreCompetency = coreCompetency;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+////    @Override
+////    public String toString() {
+////        // TODO: Implement the toString method
+////        // Start with a blank line
+////        String result = System.lineSeparator();
+////
+////        // Add labels and data for each field (you'll need to access the fields here)
+////
+////        // End with a blank line
+////        result += System.lineSeparator();
+////
+////        return result;
+////    }
+//}
--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,9 +1,12 @@
 package org.launchcode.techjobs.oo;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.*;
 public class JobTest {
     //TODO: Create your unit tests here
     private Job job1;
@@ -47,5 +50,47 @@ public class JobTest {
         // Check if the two jobs are equal
         assertFalse(job1.equals(job2));
     }
+
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        // Create a Job object with just ID
+        Job testJob = new Job();
+        // Get the string representation using the toString method
+        String expected = System.lineSeparator() +
+                "ID: " + testJob.getId() + System.lineSeparator() +
+                "Name: Data not available" + System.lineSeparator() +
+                "Employer: Data not available" + System.lineSeparator() +
+                "Location: Data not available" + System.lineSeparator() +
+                "Position Type: Data not available" + System.lineSeparator() +
+                "Core Competency: Data not available" + System.lineSeparator();
+        assertEquals(expected, testJob.toString());
+    }
+
+
+
+    @Test
+    public void testToStringContainsCorrectLabelsAndData() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        String actual = testJob.toString();
+        assertTrue(actual.contains("ID: " + testJob.getId()));
+        assertTrue(actual.contains("Name: Product tester"));
+        assertTrue(actual.contains("Employer: ACME"));
+        assertTrue(actual.contains("Location: Desert"));
+        assertTrue(actual.contains("Position Type: Quality control"));
+        assertTrue(actual.contains("Core Competency: Persistence"));
+    }
+
+    @Test
+    public void testToStringHandlesEmptyField() {
+        Job testJob = new Job();
+        String actual = testJob.toString();
+        assertTrue(actual.contains("Name: Data not available"));
+        assertTrue(actual.contains("Employer: Data not available"));
+        assertTrue(actual.contains("Location: Data not available"));
+        assertTrue(actual.contains("Position Type: Data not available"));
+        assertTrue(actual.contains("Core Competency: Data not available"));
+    }
+
+
 }
 
